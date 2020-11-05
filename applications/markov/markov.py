d9afb0ea95b2aec2061a9344c5125638dc5ed9ec
Benjamin Whitman <benjamin.whitman90@gmail.com>
2020-11-05 16:14:00 -0500

    no_dups, start markov

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -4,10 +4,27 @@ import random
 with open("input.txt") as f:
     words = f.read()
 
-# TODO: analyze which words can follow other words
-# Your code here
+words = words.split()
+
+# analyze which words can follow other words
+# If word is in markov, append idx+1 to list of followers
+# If word is not in markov, add to markov as key and init followers list with 
+# idx+1
+markov = {}
+idx = 0
+while idx < len(words)-1:
+    word = words[idx]
+    if word in list(markov):
+        followers = markov[word]
+        followers.append(words[idx+1])
+        markov[word] = followers
+    else:
+        markov[word] = [words[idx+1]]
+    idx += 1
+
+
 
 
 # TODO: construct 5 random sentences
-# Your code here
+
 
